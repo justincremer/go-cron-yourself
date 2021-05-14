8979942587a72d5965545f1de1d90871b9d2363c
justincremer <jacremer@live.com>
2021-05-14 16:30:27 -0700

    task/ was not commited for some reason

---
+++ task/command.go
@@ -0,0 +1,36 @@
+package task
+
+import (
+	"log"
+	"fmt"
+	"os/exec"
+	"strings"
+	"errors"
+)
+
+func Test(i string) {
+	cmd, err := CreateCmd(i)
+	must(err)
+	fmt.Printf("Command: %s\n", cmd.String())
+
+	res, err := cmd.Output()
+	must(err)
+	fmt.Printf("result: %s\n", res)
+}
+
+func CreateCmd(i string) (*exec.Cmd, error) {
+	switch argv := strings.Fields(i); len(argv) {
+	case 0:
+		return nil, errors.New("Error: input must not be nil")
+	case 1:
+		return exec.Command(argv[0]), nil
+	default:
+		return exec.Command(argv[0], argv[1:]...), nil
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
